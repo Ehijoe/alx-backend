756ab9986cfdbabcbf3555300004d439211c3d45
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-17 14:43:44 +0100

    0x01 - Fix task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,6 +8,8 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """Cache an item."""
+        if key is None or item is None:
+            return
         self.cache_data[key] = item
 
     def get(self, key):
